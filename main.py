8a741dd305143b159487faec95a40cef94a11603
Tunji David <tunjiobidokun@gmail.com>
2025-06-07 11:38:45 +0100

    Add optional model name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from openai import OpenAI
-from tools import tools
+from tools import tools # type: ignore
 from functions import make_purchase, order_items
 from system_prompt import system_prompt
 from dotenv import load_dotenv
@@ -35,13 +35,17 @@ response = client.chat.completions.create(
     tools=tools,
     tool_choice='auto'
 )
-
 print(response)
+print(f"AI response: {response.choices[0].message.content}")
 
 if response.choices[0].message.tool_calls is not None:
     for tool_call in response.choices[0].message.tool_calls:
         tool_name = tool_call.function.name
         tool_args = json.loads(tool_call.function.arguments)
+
+        print(f"Tool name: {tool_name}")
+        print(f"Tool arguments: {tool_args}")
+
         if tool_name == "make_purchase":
             result = make_purchase(**tool_args)
             print(result)
@@ -50,6 +54,12 @@ if response.choices[0].message.tool_calls is not None:
             print(result)
         else:
             raise ValueError(f"Unknown tool: {tool_name}")
+        
+        
+        
+
+
+
 
 
 
